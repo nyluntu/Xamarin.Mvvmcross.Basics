aaa1b4462e78c8528da917de1d00543af6a99868
Sovelluskontti <sovelluskontti@Tonis-Mac-mini.local>
2016-02-16 01:51:53 +0200

    Created ios application that uses mvvm.cross.

---
+++ Calculator.UI.iOs/Setup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MvvmCross.iOS.Platform;
+using MvvmCross.iOS.Views.Presenters;
+using MvvmCross.Core.ViewModels;
+
+namespace Calculator.UI.iOs
+{
+	public class Setup : MvxIosSetup
+	{
+		public Setup (MvxApplicationDelegate appDelegate, IMvxIosViewPresenter presenter)
+			: base (appDelegate, presenter)
+		{ }
+
+		protected override IMvxApplication CreateApp() 
+		{
+			return new App ();
+		}
+	}
+}
+
---
+++ Calculator.UI.iOs/Views/TipView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using UIKit;
+using MvvmCross.iOS.Views;
+using MvvmCross.Binding.BindingContext;
+
+namespace Calculator.UI.iOs
+{
+	public partial class TipView : MvxViewController
+	{
+		public TipView () : base ("TipView", null)
+		{
+		}
+
+		public new TipViewModel ViewModel {
+			get {return (TipViewModel)base.ViewModel; }
+			set { base.ViewModel = value; }
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			this.CreateBinding(TipLabel).To((TipViewModel vm) => vm.Tip).Apply();
+			this.CreateBinding(SubTotalTextField).To((TipViewModel vm) => vm.SubTotal).Apply();
+			this.CreateBinding(GenerositySlider).To((TipViewModel vm) => vm.Generosity).Apply();    
+		}
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
+
