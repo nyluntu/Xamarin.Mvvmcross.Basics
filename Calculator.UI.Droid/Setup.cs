455315518e2260771437a2a39559ca9337889026
Sovelluskontti <sovelluskontti@Tonis-Mac-mini.local>
2016-02-16 00:58:27 +0200

    Android ui created with mvvmcross.

---
+++ Calculator.UI.Droid/Setup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MvvmCross.Droid.Platform;
+using MvvmCross.Core.ViewModels;
+using Android.Content;
+
+namespace Calculator.UI.Droid
+{
+	public class Setup : MvxAndroidSetup
+	{
+		public Setup (Context applicationContext)
+			: base(applicationContext)
+		{
+		}
+	
+		protected override IMvxApplication CreateApp()
+		{
+			return new App ();
+		}
+	}
+
+
+}
+
---
+++ Calculator.UI.Droid/Views/TipView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using MvvmCross.Droid.Views;
+using Android.App;
+using Calculator;
+
+namespace Calculator.UI.Droid
+{
+	[Activity(Label = "Tip", MainLauncher=true)]
+	public class TipView : MvxActivity
+	{
+		public TipView ()
+		{
+		}
+
+		public new TipViewModel ViewModel {
+			get{ return (TipViewModel)base.ViewModel; }
+			set { base.ViewModel = value; }
+		}
+
+		protected override void OnViewModelSet() 
+		{
+			base.OnViewModelSet ();
+			SetContentView (Resource.Layout.View_Tip);
+		}
+	}
+}
+
